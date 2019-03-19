88e283b77af345bc4875a494161809b7af698e4e
dvsilva <Danyllo Silva>
2019-03-19 12:31:29 -0300

    creating components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
 
-alert(soma(2, 3));
+class Button extends Component {
+  render() {
+    //return <a href="">{this.props.title}</a>;
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+class App extends Component {
+  handleClick() {
+    alert("Botão clicado");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rocketseat</h1>
+        <Button title="Enviar 1" />
+        <Button onClick={this.handleClick}>Enviar 2</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
